827161aaf192079652be5b1b19711ffdc637b099
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2021-10-28 17:39:11 +0200

    added more styling/animation

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -6,7 +6,7 @@ const CharacterCard = ({ index, value, item }) => {
   return (
     <div key={index} className="column is-one-third">
       <Link to={`/${item}/${value.id}`}>
-        <div className="card">
+        <div className="card animate__animated animate__headShake">
           <div className="card-image">
             <figure className={`image ${item === 'characters' ? 'is-square' : 'is-2by1' }`}>
               <img src={value.img_url} alt={value.name}/>
